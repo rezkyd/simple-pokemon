b2b25fd938bc346fe96e338b367d7ef9f4bd83f1
Rezky Dermawan <rezky.dermawan@phincon.com>
2022-07-05 12:48:27 +0700

    feat: register route and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import "./App.css";
+import PokemonList from "./display/containers/PokemonList";
+import PokemonDetail from "./display/containers/PokemonDetail";
+import MyPokemonList from "./display/containers/MyPokemonList";
+import NotFound404 from "./display/containers/404";
+import MainBar from "./display/components/MainBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<MainBar />}>
+          <Route index element={<PokemonList />} />
+          <Route path="pokemon" element={<PokemonList />} />
+          <Route path="pokemon/:id" element={<PokemonDetail />} />
+          <Route path="mypokemon" element={<MyPokemonList />} />
+        </Route>
+        <Route path="*" element={<NotFound404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
